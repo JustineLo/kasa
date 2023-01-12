2acc31a396301239a120cac64fa5176c45507712
Justine Lo <juustine.lo@gmail.com>
2023-01-12 22:45:03 +0100

    error page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Apropos from "./view/Apropos";
 import Home from "./view/Home";
 import Logements from "./view/Logements";
+import ErrorPage from "./view/ErrorPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={ <Home/> } />
-        <Route path="logements" element={ <Logements/> } />
-        <Route path="apropos" element={ <Apropos/> } />
+        <Route path="/" element={<Home />} />
+        <Route path="logements" element={<Logements />} />
+        <Route path="apropos" element={<Apropos />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </div>
   );
