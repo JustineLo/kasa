472d9a30ea827ae1526a5d4e1637c1515da52a4c
Justine Lo <juustine.lo@gmail.com>
2023-01-13 16:04:29 +0100

    collapsible behaviour

--- src/components/Collapsible.js
+++ src/components/Collapsible.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Collapsible.module.css";
 import chevronDown from "../assets/chevronDown.svg";
 
 const Collapsible = ({ title, description }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <button className={styles.collapsible}>
+      <button className={styles.collapsible} onClick={handleClick}>
         <div className={styles.header}>
           <h1>{title}</h1>
           <img src={chevronDown} alt="chevron down" />
         </div>
-        <div className={styles.description}>
+        <div
+          className={styles.description}
+          style={{ display: isOpen ? "block" : "none" }}
+        >
           <p>{description}</p>
         </div>
       </button>
