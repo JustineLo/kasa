0a4b9b0292b80b438c0f1f797bf5dec76ed800b8
Justine Lo <juustine.lo@gmail.com>
2023-01-13 18:19:41 +0100

    handle carousel navigation

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "./Banner";
 import styles from "./Carousel.module.css";
 import chevronRight from "../assets/chevronRight.svg";
 import chevronLeft from "../assets/chevronLeft.svg";
 
 const Carousel = ({ pictures }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrevious = () => {
+    if (currentIndex === 0) {
+      setCurrentIndex(pictures.length - 1);
+    } else {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentIndex === pictures.length - 1) {
+      setCurrentIndex(0);
+    } else {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
   return (
     <>
       <div className={styles.carousel}>
-        <Banner image={pictures[1]} carousel={true} />
-        <button className={styles.chevronleft}>
+        <Banner image={pictures[currentIndex]} carousel={true} />
+        <button className={styles.chevronleft} onClick={handlePrevious}>
           <img src={chevronLeft} alt="chevron left" />
         </button>
-        <button className={styles.chevronright}>
+        <button className={styles.chevronright} onClick={handleNext}>
           <img src={chevronRight} alt="chevron right" />
         </button>
       </div>
