585b9a1e9fe7d93261e45ba7426cd64826aec186
Justine Lo <juustine.lo@gmail.com>
2023-01-13 11:20:57 +0100

    style footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -3,9 +3,12 @@ import styles from "./Footer.module.css";
 import { ReactComponent as Logo } from "../assets/footerLogo.svg";
 
 const Footer = () => {
+  const text = "© 2020 Kasa. All rights reserved";
+
   return (
     <footer className={styles.footer}>
       <Logo />
+      <p>{text}</p>
     </footer>
   );
 };
