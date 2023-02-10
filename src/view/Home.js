8a37a8a5707d705396b2f196352fee1e139eaee1
Justine Lo <juustine.lo@gmail.com>
2023-02-10 12:56:53 +0100

    fix box-sizing and font

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Banner from "../components/Banner";
 import banner from "../assets/homeBanner.png";
 import logements from "./../logements.json";
@@ -6,13 +6,41 @@ import Thumbnail from "../components/Thumbnail";
 
 const Home = () => {
   const bannerText = "Chez vous, partout et ailleurs";
+  // const [logements, setLogements] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [err, setErr] = useState(false);
+
+  // useEffect(() => {
+  //   fetch("../logements.json")
+  //     .then((res) => {
+  //       return res.json();
+  //     })
+  //     .then((json) => {
+  //       console.log(json);
+  //       setLogements([]);
+  //       setIsLoading(false);
+  //     })
+  //     .catch(() => {
+  //       setErr(true);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  // if (err) return <div>{err}</div>;
+
+  // if (isLoading) return <div>Chargement...</div>;
 
   return (
     <>
       <Banner image={banner} text={bannerText} />
       <div className="logements-container">
         {logements.map((logement) => (
-          <Thumbnail key={logement.id} id={logement.id} cover={logement.cover} title={logement.title} />
+          <Thumbnail
+            key={logement.id}
+            id={logement.id}
+            cover={logement.cover}
+            title={logement.title}
+          />
         ))}
       </div>
     </>
