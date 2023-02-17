33d2d6a36c80df58d0332113233474ba0cac56aa
Justine Lo <juustine.lo@gmail.com>
2023-02-17 11:57:22 +0100

    fix iamge size

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,10 +1,8 @@
 import React from "react";
 import styles from "./Thumbnail.module.css";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Thumbnail = ({ id, cover, title }) => {
-  const navigate = useNavigate();
-
   return (
     <>
       <Link to={`/logements/${id}`} className={styles.thumbnail}>
